e7584780644fc323c776e14f0849f73aaa27d39c
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2022-11-23 11:26:40 +0300

    Create Bankomat.h

---
+++ Lab 3/Bankomat.h
@@ -0,0 +1,28 @@
+#ifndef BANKOMAT_LAB3_OOP
+#define BANKOMAT_LAB3_OOP
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "Money.h"
+#include <sstream>
+
+class Bankomat
+{
+	int id;
+	Money last_sum;
+	Money min_sum;
+	Money max_sum;
+public:
+	Bankomat();
+	Bankomat(int, Money, Money, Money);
+	bool Check(Money);
+	void LoadMoney(Money& b);
+	void CutMoney(Money& b);
+
+	std::string toString();
+	
+	friend std::ostream& operator<<(std::ostream& stream, Bankomat a);
+};
+
+#endif
