ba71c36319e2f1310b7e093e49eec134e05cf3a8
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2022-11-23 11:25:37 +0300

    Create Lab 3.cpp

---
+++ Lab 3/Lab 3.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include "Money.h"
+#include "Bankomat.h"
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	bool flag = true;
+	while (flag)
+	{
+		double mind;
+		double maxd;
+		int id;
+		double lastd;
+		double cashd;;
+		
+		cout << "Введите идентификатор банкомата: " << std::endl;
+		cin >> id;
+		cout << "Введите минимум и максимум денег для снятия и наличия в банкомате: " << std::endl;
+		cin >> mind >> maxd;
+		cout << "Введите оставшиеся деньги в банкомате: " << std::endl;
+		cin >> lastd;
+		cout << "Введите сумму " << endl;
+		cin >> cashd;
+		
+		Money min = mind;
+		Money max = maxd;
+		Money last = lastd; 
+		Money cash = cashd;
+		Bankomat bank(id, last, min, max);
+		
+		char action;
+		bool label1 = true;
+		while (label1)
+		{
+			cout << "Что вы хотите сделать? 1 - снять деньги, 2 - пополнить счет " << endl;
+			cin >> action;
+			switch (action)
+			{
+			default:
+				std::cout << "Неверный ввод!" << std::endl;
+				label1 = true;
+				break;
+			case '1':
+				bank.CutMoney(cash);
+				label1 = false;
+				break;
+			case '2':
+				bank.LoadMoney(cash);
+				label1 = false;
+				break;
+			}
+		}
+		cout << bank << endl;
+		char choice;
+		bool label2 = true;
+		while (label2)
+		{
+			std::cout << "Желаете начать заново или выйти? 1 - заново, 2 - выйти ";
+			std::cin >> choice;
+			switch (choice)
+			{
+			default:
+				std::cout << "Неверный ввод!" << std::endl;
+				label2 = true;
+				break;
+			case '1':
+				flag = true;
+				label2 = false;
+				break;
+			case '2':
+				flag = false;
+				label2 = false;
+				break;
+			}
+		}
+	}
+}
