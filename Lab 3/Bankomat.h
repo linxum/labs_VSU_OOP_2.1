6786b8cf320e63abe16d8fedc81543d8576d455e
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2022-11-29 18:55:59 +0300

    Update Bankomat.h

--- Lab 3/Bankomat.h
+++ Lab 3/Bankomat.h
@@ -18,11 +18,12 @@ public:
 	Bankomat(int, Money, Money, Money);
 	bool Check(Money);
 	void LoadMoney(Money& b);
-	void CutMoney(Money& b);
+	void CutMoney(double);
 
 	std::string toString();
 	
 	friend std::ostream& operator<<(std::ostream& stream, Bankomat a);
+	friend std::istream& operator>>(std::istream& stream, Bankomat a);
 };
 
 #endif
