c52881c1776865962cb2a819b6c4996d3af8c322
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2022-12-03 22:07:49 +0300

    Create Pair.h

---
+++ Lab 4/Pair.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+
+class Pair
+{
+protected:
+	int a, b;
+public:
+	Pair();
+	Pair(int, int);
+
+	Pair* operator +(Pair&);
+	Pair* operator -(Pair&);
+	Pair* operator *(Pair&);
+	Pair* operator /(Pair&);
+	bool operator ==(Pair&);
+	friend std::ostream& operator <<(std::ostream&, Pair&);
+	friend std::istream& operator >>(std::istream&, Pair&);
+};
