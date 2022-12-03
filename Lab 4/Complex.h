d50272977604a9f95fbcb8cee918c007ce108329
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2022-12-03 22:08:21 +0300

    Create Complex.h

---
+++ Lab 4/Complex.h
@@ -0,0 +1,18 @@
+#include "Pair.h"
+#pragma once
+
+class Complex: public Pair
+{
+public:
+	Complex();
+	Complex(int, int);
+	Complex* operator +(Complex&);
+	Complex* operator -(Complex&);
+	Complex* operator *(Complex&);
+	Complex* operator /(Complex&);
+	bool operator ==(Complex&);
+	friend std::ostream& operator <<(std::ostream&, Complex&);
+	friend std::istream& operator >>(std::istream&, Complex&);
+
+};
+
