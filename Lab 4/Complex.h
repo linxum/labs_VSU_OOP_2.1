1659d5ec99bb1de83c6724883b0f8e39b2091cff
Ivan Smekchnev <49167389+linxum@users.noreply.github.com>
2022-12-07 16:43:23 +0300

    Update Complex.h

--- Lab 4/Complex.h
+++ Lab 4/Complex.h
@@ -6,11 +6,10 @@ class Complex: public Pair
 public:
 	Complex();
 	Complex(int, int);
-	Complex* operator +(Complex&);
-	Complex* operator -(Complex&);
+
 	Complex* operator *(Complex&);
-	Complex* operator /(Complex&);
 	bool operator ==(Complex&);
+
 	friend std::ostream& operator <<(std::ostream&, Complex&);
 	friend std::istream& operator >>(std::istream&, Complex&);
 
